ce5b3815ee7e8fe3fcb24b013c1beab2e2f6693f
Ioana Seres <ioana.cosoreanuseres@888holdings.com>
2022-05-24 15:48:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const newYears = "1 Jan 2023";
+const daysEl = document.querySelector(".days");
+const hoursEl = document.querySelector(".hours");
+const minutesEl = document.querySelector(".minutes");
+const secondsEl = document.querySelector(".seconds");
+
+function countDown() {
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 86400);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+  console.log(days, hours, minutes, seconds);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+countDown();
+
+setInterval(countDown, 1000);
